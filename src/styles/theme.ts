2ddf1393337f1cb6c02f0f7b948dd4c47cdded43
h-0y28 <s24015@gsm.hs.kr>
2024-11-21 09:52:33 +0900

    style: change colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,9 @@
 export const theme = {
   colors: {
-    primary: "#F28482", // 메인 색상 - 핑크
+    primary: "#857F74", // 메인 색상 - 핑크
     secondary: "#0A090C", // 보조 색상 - 검정
     background: "#F7EDE2", // 배경 색상
-    // accent: "#", // 강조 색상
+    accent: "#033F63", // 강조 색상 - 파랑
     // highlight: "#", // 포인트 색상 - 베이지
   },
   fonts: {
