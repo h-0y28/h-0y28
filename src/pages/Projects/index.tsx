bd4ea25fa01605c2d4b3339ddb4c8aa2667c7424
h-0y28 <s24015@gsm.hs.kr>
2024-11-22 08:22:53 +0900

    init: 임시 project section

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,3 +1,41 @@
-export default function Skills() {
-  return <>Skills</>;
+import ProjectCard from "./../../components/ProjectCard/index";
+import * as S from "./style";
+import * as T from "../style";
+
+export default function Projects() {
+  const projects = [
+    {
+      skills: ["React", "TypeScript", "Tailwind CSS"],
+      title: "Project 1",
+      content: "This is the first project description.",
+      href: "https://example1.com",
+    },
+    {
+      skills: ["Next.js", "Chakra UI", "GraphQL"],
+      title: "Project 2",
+      content: "This is the second project description.",
+      href: "https://example2.com",
+    },
+    {
+      skills: ["Vue", "Vuetify", "Firebase"],
+      title: "Project 3",
+      content: "This is the third project description.",
+      href: "https://example3.com",
+    },
+  ];
+
+  return (
+    <T.Container>
+      <S.Title>Projects</S.Title>
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          skills={project.skills}
+          title={project.title}
+          content={project.content}
+          href={project.href}
+        />
+      ))}
+    </T.Container>
+  );
 }
---
+++ src/pages/Projects/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { theme } from "./../../styles/theme";
+
+export const Title = styled.div`
+  font-size: 5rem;
+  font-weight: 700;
+  color: ${theme.colors.background};
+`;
