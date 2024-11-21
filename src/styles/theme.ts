1f1e4e1394da92ff8b117334a6cb5830f8364cec
h-0y28 <s24015@gsm.hs.kr>
2024-11-22 00:25:20 +0900

    style: change colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,12 @@
 export const theme = {
   colors: {
-    primary: "#1F487E", // 메인 색상 - 파랑
-    secondary: "#95B8D1", // 보조 색상 - 하늘
+    primary: "#FFC300", // 메인 색상 - 노랑
+    secondary: "#006400", // 보조 색상 - 하늘
     background: "#F7EDE2", // 배경 색상
     black: "#17171B",
     white: "#ffffff",
-    // accent: "#033F63", // 강조 색상 - 파랑
-    // highlight: "#", // 포인트 색상 - 베이지
+    highlight: "#F6AE2D", // 포인트 색상 - 노랑
+    // accent: "#FFC300", // 강조 색상 - 노랑
   },
   fonts: {
     heading: "Arial, sans-serif",
