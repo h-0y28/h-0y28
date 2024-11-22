1401350446e3554dc97984a00aaaf2be785c4074
h-0y28 <s24015@gsm.hs.kr>
2024-11-22 10:26:31 +0900

    add: gray color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,9 +3,10 @@ export const theme = {
     primary: "#FFC300", // 메인 색상 - 노랑
     secondary: "#006400", // 보조 색상 - 하늘
     background: "#F7EDE2", // 배경 색상
-    black: "#17171B",
-    white: "#ffffff",
     highlight: "#FF8C00 ", // 포인트 색상 - 주황
+    black: "#17171B",
+    gray: "#B0B0B0",
+    white: "#ffffff",
     // accent: "#FFC300", // 강조 색상 - 노랑
   },
   fonts: {
