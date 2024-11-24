12a61fca97be5bdb0a49c8c406b2f87c3646074d
h-0y28 <s24015@gsm.hs.kr>
2024-11-24 11:44:01 +0900

    style: change style - animaition

--- src/components/AboutCard/style.ts
+++ src/components/AboutCard/style.ts
@@ -1,32 +1,45 @@
 import styled from "styled-components";
 import theme from "../../styles/theme";
 
-export const CardContainer = styled.div`
-  border: 1px solid ${theme.colors.highlight};
-  border-radius: 12px;
-  padding: 20px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.08);
-  width: 18rem;
-  height: 9rem;
-  margin: 16px;
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-  cursor: pointer;
+export const Container = styled.div`
+  border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
 
+export const CardContainer = styled.div`
+  border-bottom: 1px solid ${theme.colors.highlight};
+  padding-top: 12px;
+  padding-right: 12px;
+  padding-bottom: 0;
+  width: 15rem;
+  height: 4.5rem;
+  margin: 10px;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+  transition: border-color 0.5s ease;
+  cursor: pointer;
   &:hover {
-    transform: scale(1.05);
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+    border-bottom: 1.5px solid ${theme.colors.black};
   }
 `;
 
 export const CardTitle = styled.h3`
-  font-size: 1.5rem;
+  font-size: 0.8rem;
   color: ${theme.colors.black};
-  margin-bottom: 8px;
+  margin-bottom: 4px;
   font-weight: bold;
 `;
 
 export const CardContent = styled.p`
-  font-size: 1rem;
+  font-size: 0.5rem;
   color: #666;
-  line-height: 1.5;
+  line-height: 1.3;
+`;
+
+export const Image = styled.img`
+  margin-left: 1rem;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 12px;
 `;
