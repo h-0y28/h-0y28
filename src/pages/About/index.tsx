c9b5fa46ac93120629707ffca1f2a154a7698748
h-0y28 <s24015@gsm.hs.kr>
2024-11-25 10:27:40 +0900

    style: change code style

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -9,63 +9,57 @@ import github from "../../assets/github.png";
 import linkedIn from "../../assets/linkedin.png";
 import email from "../../assets/email.png";
 
+const aboutData = [
+  {
+    title: "생년월일",
+    content: "2008.08.02",
+    imageSrc: birthday,
+  },
+  {
+    title: "학교",
+    content: "광주소프트웨어마이스터고(2023.03.04~2027.01)",
+    href: "https://www.instagram.com/gsmschool_official/",
+    imageSrc: school,
+  },
+  {
+    title: "전화번호",
+    content: "010-5496-1458",
+    imageSrc: phone,
+  },
+  {
+    title: "GitHub",
+    content: "내 GitHub",
+    href: "https://github.com/h-0y28",
+    imageSrc: github,
+  },
+  {
+    title: "LinkedIn",
+    content: "내 LinkedIn",
+    href: "https://www.linkedin.com/in/%ED%9A%A8%EC%A3%BC-%EC%A0%95-80242931a/",
+    imageSrc: linkedIn,
+  },
+  {
+    title: "이메일",
+    content: "fedevluna@gmail.com",
+    href: "mailto:fedevluna@gmail.com",
+    imageSrc: email,
+  },
+];
+
 export default function About() {
   return (
     <T.Container>
       <S.Title>About</S.Title>
       <S.CardContainer>
-        <AboutCard title="생년월일" content="2008.08.02" imageSrc={birthday} />
-        {/* <a href="https://www.flaticon.com/kr/free-icons/" title="달력 아이콘">
-          달력 아이콘 제작자: Tanah Basah - Flaticon
-        </a> */}
-        <AboutCard
-          title="학교"
-          content="광주소프트웨어마이스터고(2023.03.04~2027.01)"
-          href="https://www.instagram.com/gsmschool_official/"
-          imageSrc={school}
-        />
-        {/* <a href="https://www.flaticon.com/kr/free-icons/" title="학교 아이콘">
-          학교 아이콘 제작자: Freepik - Flaticon
-        </a> */}
-
-        <AboutCard title="전화번호" content="010-5496-1458" imageSrc={phone} />
-        {/* <a
-          href="https://www.flaticon.com/kr/free-icons/-"
-          title="전화 번호 아이콘"
-        >
-          전화 번호 아이콘 제작자: Freepik - Flaticon
-        </a> */}
-        <AboutCard
-          title="GitHub"
-          content="내 GitHub"
-          href="https://github.com/h-0y28"
-          imageSrc={github}
-        />
-        {/* <a
-          href="https://www.flaticon.com/kr/free-icons/github"
-          title="github 아이콘"
-        >
-          Github 아이콘 제작자: Freepik - Flaticon
-        </a> */}
-        <AboutCard
-          title="LinkedIn"
-          content="내 LinkedIn"
-          href="https://www.linkedin.com/in/%ED%9A%A8%EC%A3%BC-%EC%A0%95-80242931a/"
-          imageSrc={linkedIn}
-        />
-        {/* <AboutCard
-          title="블로그"
-          content="내 블로그"
-          href="https://velog.io/@jhj080802/posts"
-        /> */}
-        <AboutCard
-          title="이메일"
-          content="fedevluna@gmail.com"
-          imageSrc={email}
-        />
-        {/* <a href="https://www.flaticon.com/kr/free-icons/" title="이메일 아이콘">
-          이메일 아이콘 제작자: Freepik - Flaticon
-        </a> */}
+        {aboutData.map((item, index) => (
+          <AboutCard
+            key={index}
+            title={item.title}
+            content={item.content}
+            href={item.href}
+            imageSrc={item.imageSrc}
+          />
+        ))}
       </S.CardContainer>
     </T.Container>
   );
