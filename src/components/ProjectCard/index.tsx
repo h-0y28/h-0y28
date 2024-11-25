6bc28b4f00b7c21a1bad12cc6fe48fddf2d8dd9d
h-0y28 <s24015@gsm.hs.kr>
2024-11-25 10:09:08 +0900

    feat: interface 요소 추가, 코드 스타일 변경

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,31 +1,65 @@
 import React from "react";
 import * as S from "./style";
 
+import GithubIcon from "../../assets/githubColor.png";
+import WebsiteIcon from "../../assets/websiteColor.png";
+
 interface CardProps {
   skills: string[];
   title: string;
   content: string;
   href: string;
+  term: string;
+  githubHref?: string;
+  websiteHref?: string;
 }
 
-const ProjectCard: React.FC<CardProps> = ({ skills, title, content, href }) => {
+const ProjectCard: React.FC<CardProps> = ({
+  skills,
+  title,
+  content,
+  href,
+  term,
+  githubHref,
+  websiteHref,
+}) => {
+  // console.log("GithubIcon:", GithubIcon);
+  // console.log("WebsiteIcon:", WebsiteIcon);
+
   return (
-    <a
+    <S.CardContainer
+      as="a"
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      style={{ textDecoration: "none" }}
     >
-      <S.CardContainer>
+      <S.Header>
+        <S.Term>{term}</S.Term>
+        {/* 깃 + 웹 링크 */}
+        <S.IntroduceContainer>
+          {githubHref && (
+            <a href={githubHref} target="_blank" rel="noopener noreferrer">
+              <S.GithubImg src={GithubIcon} alt="GitHub Icon" />
+            </a>
+          )}
+          {websiteHref && (
+            <a href={websiteHref} target="_blank" rel="noopener noreferrer">
+              <S.WebsiteImg src={WebsiteIcon} alt="Website Icon" />
+            </a>
+          )}
+        </S.IntroduceContainer>
+      </S.Header>
+
+      <S.ContentContainer>
+        <S.CardTitle>{title}</S.CardTitle>
         <S.StackContainer>
           {skills.map((skill, index) => (
             <S.Stack key={index}>{skill}</S.Stack>
           ))}
         </S.StackContainer>
-        <S.CardTitle>{title}</S.CardTitle>
         <S.CardContent>{content}</S.CardContent>
-      </S.CardContainer>
-    </a>
+      </S.ContentContainer>
+    </S.CardContainer>
   );
 };
 
--- src/components/ProjectCard/style.ts
+++ src/components/ProjectCard/style.ts
@@ -5,12 +5,11 @@ export const CardContainer = styled.div`
   border: 1px solid ${theme.colors.highlight};
   border-radius: 12px;
   padding: 20px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.08);
-  width: 18rem;
-  height: 9rem;
-  margin: 16px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  width: 80%;
   transition: transform 0.3s ease, box-shadow 0.3s ease;
   cursor: pointer;
+  background-color: ${theme.colors.background};
 
   &:hover {
     transform: scale(1.05);
@@ -18,9 +17,55 @@ export const CardContainer = styled.div`
   }
 `;
 
-export const StackContainer = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
 
-export const Stack = styled.div``;
+export const Term = styled.div`
+  font-size: 0.9rem;
+  color: ${theme.colors.black};
+  font-weight: bold;
+`;
+
+export const IntroduceContainer = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const GithubImg = styled.img`
+  width: 1.7rem;
+  height: 1.7rem;
+`;
+
+export const WebsiteImg = styled.img`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const StackContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  margin-bottom: 8px;
+`;
+
+export const Stack = styled.div`
+  padding: 4px 8px;
+  background-color: ${theme.colors.highlight};
+  color: ${theme.colors.background};
+  border-radius: 4px;
+  font-size: 0.8rem;
+  font-weight: bold;
+`;
 
 export const CardTitle = styled.h3`
   font-size: 1.5rem;
