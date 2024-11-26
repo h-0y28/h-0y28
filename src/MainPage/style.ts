407861168ecf168d0b814672a17edcbe40fe1130
h-0y28 <s24015@gsm.hs.kr>
2024-11-26 20:50:18 +0900

    style: main page

--- src/MainPage/style.ts
+++ src/MainPage/style.ts
@@ -1,13 +1,41 @@
 import styled from "styled-components";
 
-export const Section = styled.section<{ bgColor?: string; bgImg?: string }>`
-  height: 100vh;
-  background-color: ${(props) =>
-    props.bgImg ? "transparent" : props.bgColor || "transparent"};
-  background-image: ${(props) =>
-    props.bgImg ? `url(${props.bgImg})` : "none"};
-  background-size: cover;
-  background-position: center;
-  display: flex;
-  justify-content: center;
+interface SectionProps {
+  bgImg?: string;
+  bgColor?: string;
+}
+export const Section = styled.section<SectionProps>`
+  position: relative;
+
+  ${({ bgImg }) =>
+    bgImg &&
+    `
+      background-image: url(${bgImg});
+      background-size: cover;
+      background-position: center;
+    `}
+
+  ${({ bgColor }) =>
+    bgColor &&
+    `
+      background-color: ${bgColor};
+    `}
+
+  /* 홈 섹션에만 배경 불투명도 적용 */
+  &:nth-of-type(1) {
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 0; /* 다른 콘텐츠보다 뒤에 표시 */
+    }
+    * {
+      position: relative;
+      z-index: 1;
+    }
+  }
 `;
