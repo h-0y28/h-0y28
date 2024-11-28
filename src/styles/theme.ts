fdeba9a1825af53b54bbc9f20a3b29ee21d54e9b
h-0y28 <s24015@gsm.hs.kr>
2024-11-29 00:00:52 +0900

    style: add color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,7 @@ export const theme = {
     secondary: "#006400", // 보조 색상 - 하늘
     background: "#F7EDE2", // 배경 색상
     highlight: "#FF8C00 ", // 포인트 색상 - 주황
+    backgroundLight: "#FFFFFF",
     black: "#17171B",
     gray: "#B0B0B0",
     white: "#ffffff",
