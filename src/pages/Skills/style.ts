20bb226d3ea41d802766d7a85a27ea528c62c6fa
h-0y28 <s24015@gsm.hs.kr>
2024-12-06 08:00:57 +0900

    style: increase font size

--- src/pages/Skills/style.ts
+++ src/pages/Skills/style.ts
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 import theme from "../../styles/theme";
 
-export const Title = styled.div`
-  color: ${theme.colors.primary};
-  text-shadow: 2px 2px 5px ${theme.colors.highlight};
-  text-align: center;
-  font-size: 4rem;
-  padding-top: 5rem;
-  font-weight: bold;
-  margin-bottom: 2rem;
-`;
-
 export const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 2rem;
   justify-content: center;
   padding: 5rem 2rem;
+`;
+
+export const Title = styled.div`
+  color: ${theme.colors.primary};
+  text-shadow: 2px 2px 5px ${theme.colors.highlight};
+  text-align: center;
+  font-size: 6rem;
+  padding-top: 5rem;
+  font-weight: bold;
+  margin-bottom: 2rem;
 `;
