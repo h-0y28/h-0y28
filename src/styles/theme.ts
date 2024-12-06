c152c126ee4fb8a97a81042ca62a2731e258c872
h-0y28 <s24015@gsm.hs.kr>
2024-12-06 16:04:03 +0900

    chore: 필요 없는 코드 제거

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,12 +4,10 @@ export const theme = {
     secondary: "#006400", // 보조 색상 - 하늘
     background: "#F7EDE2", // 배경 색상
     highlight: "#FF8C00 ", // 포인트 색상 - 주황
-    backgroundLight: "#FFFFFF",
     black: "#17171B",
     gray: "#B0B0B0",
     white: "#ffffff",
     silver: "#C0C0C0",
-    // accent: "#FFC300", // 강조 색상 - 노랑
   },
   fonts: {
     heading: "Arial, sans-serif",
